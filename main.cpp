e371795b37cf68dd73f78eee79c51375aaab36f6
senayt gobezie <86910216+senaytgobezie@users.noreply.github.com>
2022-02-18 03:32:37 -0800

    imported files needed

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>//for getch()
+#include <iomanip>//for setw()
+#include<fstream>//for file write()
+#include<ios>//stream size
+#include<limits>//numeric limits
+#include<windows.h>//for editing display
+using namespace std;
+
+//structure of a registered course, a student and a course
+struct Reg_course
+{
+    int reg_courseNo;
+    string grade;
+};
+
+struct Student
+{
+	int id;
+	string fname;
+	string lname;
+	int age;
+	char sex;
+	Reg_course reg_course[50];
+    Student *snext;
+
+};
+
+struct Course ee
+{
+    int courseNo;
+    int creditHr;
+    string courseName;
+    Course *cnext;
+};
